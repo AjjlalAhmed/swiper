40e956864657cea0681f532a382adc90e0c7afae
Ajjlal Ahmed <ajjlalahmed48@gmail.com>
2025-07-30 22:35:12 +0500

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -7,6 +7,13 @@
         <div class="swiper-slide">Slide 3</div>
         <div class="swiper-slide">Slide 4</div>
         <div class="swiper-slide">Slide 5</div>
+        <div class="swiper-slide">Slide 6</div>
+        <div class="swiper-slide">Slide 7</div>
+        <div class="swiper-slide">Slide 6</div>
+        <div class="swiper-slide">Slide 7</div>
+        <div class="swiper-slide">Slide 8</div>
+        <div class="swiper-slide">Slide 9</div>
+        <div class="swiper-slide">Slide 10</div>
       </div>
 
       <!-- Navigation buttons -->
@@ -17,34 +24,29 @@
 </template>
 
 <script setup>
-import { onMounted } from "vue";
-import Swiper from "swiper/bundle";
-import "swiper/css/bundle";
+import { onMounted } from 'vue'
+import Swiper from 'swiper/bundle'
+import 'swiper/css/bundle'
 
 onMounted(() => {
-  new Swiper(".swiper-container", {
-    slidesPerView: 3,
-    spaceBetween: 30,
+  new Swiper('.swiper-container', {
+    slidesPerView: 5,
     centeredSlides: true,
-    roundLengths: true,
     loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
+    initialSlide: 0, 
+    spaceBetween: 10,
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
-  });
-});
+  })
+})
 </script>
 
 <style scoped>
 .swiper-container {
   width: 90vw;
   height: 300px;
-  position: relative;
   margin: 2rem auto;
   overflow: hidden;
 }
@@ -55,8 +57,9 @@ onMounted(() => {
   display: flex;
   align-items: center;
   justify-content: center;
-  transform: scale(0.8);
+  transform: scale(0.85);
   transition: all 200ms ease-in-out;
+  border-radius: 12px;
 }
 
 .swiper-slide.swiper-slide-active {
